28e4759d60060e83c63791fe9ad0aaa7238fc9c8
codyNog <yoneyoneyone99@gmail.com>
2021-04-12 21:21:08 +0900

    テスト追加

---
+++ __tests__/store/organisms/Counter/index.test.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "~/store";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useCounter } from "~/store/hooks/organisms/Counter";
+
+const wrapper: React.ComponentType = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+describe("useCounter", () => {
+  test("初期状態", () => {
+    const { result } = renderHook(() => useCounter(), { wrapper });
+    expect(result.current.count).toBe(0);
+  });
+
+  test("インクリメント", () => {
+    const { result } = renderHook(() => useCounter(), { wrapper });
+
+    act(() => {
+      result.current.onClickIncrementButton();
+    });
+
+    expect(result.current.count).toBe(1);
+  });
+
+  test("デクリメント", () => {
+    const { result } = renderHook(() => useCounter(), { wrapper });
+
+    act(() => {
+      result.current.onClickDecrementButton();
+    });
+
+    expect(result.current.count).toBe(0);
+  });
+});
