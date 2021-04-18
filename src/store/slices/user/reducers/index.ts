f3789e6305db970c561b8cf7b9f6f7cd810a4b62
codyNog <yoneyoneyone99@gmail.com>
2021-04-18 22:42:06 +0900

    アノテーション追加

--- src/store/slices/user/reducers/index.ts
+++ src/store/slices/user/reducers/index.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { User } from "~/domain/entities/User";
 
-const fetchUser = createAsyncThunk("fetchUser", async (id: string) => {
-  return { id, name: "bar", age: 20 };
-});
+const fetchUser = createAsyncThunk(
+  "fetchUser",
+  async (id: string): Promise<User> => {
+    return { id, name: "bar", age: 20 };
+  }
+);
 
 export const userAsyncReducers = {
   fetchUser
